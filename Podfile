96cd5caf84817bcb07a76ed1c81afcabdc0e3cd4
Ruslan Syanov <rsyanov@gmail.com>
2018-09-22 21:38:59 +0300

    Base structure

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '9.0'
+
+target 'ViperFlickrViewer' do
+  use_frameworks!
+end
---
+++ ViperFlickrViewer/Classes/PresentationLayer/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViperFlickrViewer
+//
+//  Created by Ruslan Syanov on 22.09.2018.
+//  Copyright © 2018 Ruslan Syanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
